649297f3be405f15f4c437b16b837572e54c471f
Luuk Verhagen <luukverhagen96@gmail.com>
2021-02-03 12:06:58 +0100

    It works

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("this must work");
+        System.out.println("It works");
     }
 }
