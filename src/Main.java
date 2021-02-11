3bdeb36a3952f0717b6c02dd557e690d63205ca1
Tom <tom.martens01@gmail.com>
2021-02-11 12:39:09 +0100

    fxgrapics2d library added

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
+import static javafx.application.Application.launch;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
-        System.out.println("this must yes");
-        System.out.println("It works");
-        System.out.println("HALLO");
-        for (int i = 0; i < 100; i++) {
-            test();
-        }
+        //start program
+
+        startGUI();
     }
 
-    public static void test() {
-        System.out.println("you have tested");
+    public static void startGUI(){
+        launch(MainView.class);
     }
 }
