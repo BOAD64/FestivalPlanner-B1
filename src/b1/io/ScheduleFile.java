554737ed92857a50f27620259240166360092ae3
Lieselotte <l.sihasale@xs4all.nl>
2021-02-18 10:55:19 +0100

    Added ScheduleFile logic and empty Schedule class

---
+++ src/b1/io/ScheduleFile.java
@@ -0,0 +1,60 @@
+package b1.io;
+
+import b1.schedule.Schedule;
+
+import java.io.*;
+
+public class ScheduleFile {
+
+    private static String filePath;
+    private static Schedule schedule;
+
+    public static Schedule getSchedule() {
+        if (schedule == null) {
+            if(filePath.isEmpty()){
+                return null;
+            }
+            schedule = readFile(filePath);
+        }
+        return schedule;
+    }
+
+    public static void save(){
+        if(filePath.isEmpty()){
+           return;
+        }
+        writeFile(filePath, schedule);
+    }
+
+    public static String getFilePath() {
+        return filePath;
+    }
+
+    public static void setFilePath(String filePath) {
+        ScheduleFile.filePath = filePath;
+    }
+
+    public static void setSchedule(Schedule schedule) {
+        ScheduleFile.schedule = schedule;
+    }
+
+    private static Schedule readFile(String fileName) {
+        File file = new File(fileName);
+        if(!file.exists()) {
+            try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(fileName))) {
+                return (Schedule) objectInputStream.readObject();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private static void writeFile(String filename, Schedule schedule) {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filename))) {
+            objectOutputStream.writeObject(schedule);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
