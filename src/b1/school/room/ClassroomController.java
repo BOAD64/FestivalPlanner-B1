feb8e3db868ce6d405a48fee882c0ea201a383af
Jeroen <j.visser10@student.avans.nl>
2021-03-09 16:22:58 +0100

    Added try catch in ClassroomController for invalid input

--- src/b1/school/room/ClassroomController.java
+++ src/b1/school/room/ClassroomController.java
@@ -1,6 +1,7 @@
 package b1.school.room;
 
 import b1.Controller;
+import b1.ErrorMessage;
 import b1.io.SchoolFile;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -40,16 +41,23 @@ public class ClassroomController implements Controller {
     }
 
     private void okButton(){
-        this.applyButton();
-        this.getClassroomView().getStage().close();
+        if(this.applyButton()) {
+            this.getClassroomView().getStage().close();
+        }
     }
 
-    private void applyButton(){
-        this.classroom.setCapacity(Integer.parseInt(this.classroomView.getClassroomCapacity().getText()));
-        this.classroom.setRoomCode(this.classroomView.getClassroomCode().getText());
-        this.classroom.setLength(Double.parseDouble(this.getClassroomView().getClassroomLength().getText()));
-        this.classroom.setWidth(Double.parseDouble(this.getClassroomView().getClassroomWidth().getText()));
-        SchoolFile.getSchool().addClassroom(this.classroom);
+    private boolean applyButton(){
+        try {
+            this.classroom.setCapacity(Integer.parseInt(this.classroomView.getClassroomCapacity().getText()));
+            this.classroom.setRoomCode(this.classroomView.getClassroomCode().getText());
+            this.classroom.setLength(Double.parseDouble(this.getClassroomView().getClassroomLength().getText()));
+            this.classroom.setWidth(Double.parseDouble(this.getClassroomView().getClassroomWidth().getText()));
+            SchoolFile.getSchool().addClassroom(this.classroom);
+        } catch(Exception e) {
+            ErrorMessage.show();
+            return false;
+        }
+        return true;
     }
 
 }
