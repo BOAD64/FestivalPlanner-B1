564ba2d13f283aa258a087c32c28486972439a2b
Chunky <casperlous@outlook.com>
2021-03-25 10:45:13 +0100

    Added documentation to clock class, aswell as some minor fixes.

--- src/b1/simulation/Clock.java
+++ src/b1/simulation/Clock.java
@@ -37,8 +37,8 @@ public class Clock {
     }
 
     /**
-     * gets the current time
-     * @return
+     * Gets the current time
+     * @return current time
      */
     public LocalTime getCurrentTime(){
         return this.currentTime;
@@ -47,16 +47,16 @@ public class Clock {
     /**
      * gets the new Delta time
      * used to apply speed multiplier to delta time in simulation class
-     * @param deltaTime
-     * @return
+     * @param deltaTime deltaTime provided by animation timer
+     * @return altered deltaTime value to be used by all update methods, excluding that of the clock itself.
      */
     public double getNewDeltaTime(double deltaTime){
-        return deltaTime * speedMultiplier;
+        return deltaTime * this.speedMultiplier;
     }
 
     /**
      * sets the speed multiplier to the value
-     * @param speedMultiplier
+     * @param speedMultiplier value speedMultiplier is to be set to.
      */
     public void setSpeedMultiplier(double speedMultiplier){
         this.speedMultiplier = speedMultiplier;
@@ -64,42 +64,51 @@ public class Clock {
 
     /**
      * updates the current time to the given value
-     * @param originalDeltaTime
+     * @param originalDeltaTime deltaTime provided by animation timer, not passed through getNewDeltaTime().
      */
     public void update(double originalDeltaTime){
-        this.currentTime = this.currentTime.plusNanos((long)((originalDeltaTime * Math.pow(10, 9)) * 10 * speedMultiplier));
+        this.currentTime = this.currentTime.plusNanos((long)((originalDeltaTime * Math.pow(10, 9)) * 10 * this.speedMultiplier));
     }
 
+    /**
+     * Called when pause button is presses, checks if the simulation is paused. If not, the simulation enters
+     * the paused state. If yes, the simulation is unpaused.
+     * Pausing is done by setting the speedMultiplier to zero, so updating this variable should be prevented while paused.
+     */
     public void pause(){
-        if (!paused){
-            paused = true;
+        if (!this.paused){
+            this.paused = true;
             this.fontColor = Color.red;
-            this.previousSpeedMultiplier = speedMultiplier;
+            this.previousSpeedMultiplier = this.speedMultiplier;
             this.speedMultiplier = 0;
 
         }else {
-            paused = false;
+            this.paused = false;
             this.fontColor = Color.white;
-            speedMultiplier = previousSpeedMultiplier;
+            this.speedMultiplier = this.previousSpeedMultiplier;
         }
     }
 
+    /**
+     * Returns whether or not simulation is paused. Can be used for preventing updating of speedMultiplier in paused state.
+     * @return Boolean value; true if paused, false if not paused
+     */
     public boolean isPaused(){
-        return paused;
+        return this.paused;
     }
 
     /**
      * draws the clock with the current time
-     * @param graphics
-     * @param location
+     * @param graphics used for drawing the clock to the screen
+     * @param location the location where the clock needs to be drawn
      */
     public void draw(FXGraphics2D graphics, Point2D location){
         this.location = location;
-        graphics.setColor(fontColor);
-        graphics.setFont(clockFont);
-        String time = currentTime.format(DateTimeFormatter.ofPattern("HH:mm"));
+        graphics.setColor(this.fontColor);
+        graphics.setFont(this.clockFont);
+        String time = this.currentTime.format(DateTimeFormatter.ofPattern("HH:mm"));
         graphics.drawString(time, (float)location.getX(), (float)location.getY());
-        Shape textOutline = clockFont.createGlyphVector(graphics.getFontRenderContext(), time).getOutline();
+        Shape textOutline = this.clockFont.createGlyphVector(graphics.getFontRenderContext(), time).getOutline();
         graphics.setColor(Color.black);
         graphics.draw(AffineTransform.getTranslateInstance(location.getX(),location.getY()).createTransformedShape(textOutline));
     }
